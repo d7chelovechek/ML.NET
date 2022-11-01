59d77e608df71ad3da13d2a4e071e09c5b2f8ca8
Ayrat Khaydarov <info.chelovechek@gmail.com>
2022-11-01 16:31:37 +0300

    Task #5
    ● Implemented classifier algorithm.

---
+++ Classifier/Algorithm.cs
@@ -0,0 +1,71 @@
+﻿namespace Classifier
+{
+    internal class Algorithm
+    {
+        private readonly Dictionary<int, double> _probabilities;
+        private readonly Dictionary<int, Dictionary<int, double>> _conditionalProbabilities;
+
+        internal Algorithm()
+        {
+            _probabilities = new Dictionary<int, double>();
+            _conditionalProbabilities = new Dictionary<int, Dictionary<int, double>>();
+        }
+
+        public void Start(
+            List<List<double>> symptoms,
+            List<int> diseases,
+            int patientsCount)
+        {
+            for (var index = 0; index < diseases.Count; index++)
+            {
+                _probabilities.Add(index, (double)diseases[index] / patientsCount);
+            }
+
+            for (var index = 0; index < symptoms.Count; index++)
+            {
+                _conditionalProbabilities.Add(index, new Dictionary<int, double>());
+
+                for (var temp = 0; temp < symptoms[index].Count; temp++)
+                {
+                    _conditionalProbabilities[index].Add(temp, symptoms[index][temp]);
+                }
+            }
+        }
+
+        public string GetDisease(List<int> probabilities, List<string> diseasesNames)
+        {
+            var index = -1;
+            var maxProb = double.MinValue;
+
+            foreach (int key in _probabilities.Keys)
+            {
+                var prob = GetDiseaseResult(probabilities, key);
+
+                if (prob >= maxProb)
+                {
+                    index = key;
+                    maxProb = prob;
+                }
+            }
+
+            return diseasesNames[index];
+        }
+
+        private double GetDiseaseResult(
+            List<int> probabilities, 
+            int index)
+        {
+            var result = _probabilities[index];
+
+            for (var temp = 0; temp < probabilities.Count; temp++)
+            {
+                if (probabilities[temp] is 1)
+                {
+                    result *= _conditionalProbabilities[temp][index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Classifier/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Classifier
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var symptoms = File.ReadAllLines("Files/symptom.csv").ToList();
+            var diseases = File.ReadAllLines("Files/disease.csv").ToList();
+            
+            var diseasesNames = symptoms[0].Split(';').Skip(1).ToList();
+            var patientsCount = Convert.ToInt32(diseases[^1].Split(';')[1]);
+
+            symptoms = symptoms.Skip(1).ToList();
+
+            var diseasesNamesCount = diseasesNames.Count;
+            var symptomsCount = symptoms.Count;
+
+            var diseasesMatrix = 
+                    diseases
+                    .Skip(1)
+                    .SkipLast(1)
+                    .Select(d => Convert.ToInt32(d.Split(';')[1]))
+                    .ToList();
+
+            var symptomsMatrix = new List<List<double>>();
+            for (var index = 0; index < symptomsCount; index++)
+            {
+                symptomsMatrix.Add(
+                    symptoms[index]
+                        .Split(';')
+                        .Skip(1)
+                        .Select(s => Convert.ToDouble(s.Replace('.', ',')))
+                        .ToList());
+            }
+
+            var probabilities = new List<int>();
+            for (var index = 0; index < symptomsCount; index++)
+            {
+                _ = int.TryParse(Console.ReadLine(), out int result);
+
+                probabilities.Add(result > 1 ? 1 : result < 0 ? 0 : result);
+            }
+
+            var algorithm = new Algorithm();
+            algorithm.Start(
+                symptomsMatrix,
+                diseasesMatrix, 
+                patientsCount);
+
+            Console.WriteLine(
+                algorithm.GetDisease(
+                    probabilities, 
+                    diseasesNames));
+        }
+    }
+}
